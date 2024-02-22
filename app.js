050579d9344c17060eb8bf82ff2895dd5b849ceb
Piotr Bogacz <piotrekbogacz8@gmail.com>
2024-02-22 23:01:46 +0100

    update

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get('/apple-app-site-association', (req, res) => {
+    res.sendFile(__dirname + '/apple-app-site-association');
+});
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
