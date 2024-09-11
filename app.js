564daceb1cbfb1753b4f6de8bc790cf3983966e5
piotrbogacz <piotrek@selmo.io>
2024-09-11 15:31:02 +0200

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const path = require("path");
 
 // app.get('/apple-app-site-association', (req, res) => {
 //     res.setHeader('Content-Type', 'application/json');
