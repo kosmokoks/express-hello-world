986617889295e3ac57537734f7f1db6164f9750e
piotrbogacz <piotrek@selmo.io>
2024-09-11 15:21:47 +0200

    update

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/apple-app-site-association', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.sendFile(__dirname + '/apple-app-site-association');
+// app.get('/apple-app-site-association', (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.sendFile(__dirname + '/apple-app-site-association');
+// });
+
+app.use("/.well-known", express.static(path.join(__dirname, ".well-known")));
+app.get("/.well-known/apple-app-site-association", (req, res) => {
+    res.sendFile( path.join(__dirname, ".well-known/apple-app-site-association.json"));
 });
+
 app.get("/", (req, res) => res.type('html').send(html));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
